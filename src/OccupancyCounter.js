46f8bcc07277146d0cf6a38ded21cb3e5a15cf64
Julian De Angelis <julian.deangelis@gmail.com>
2020-12-20 20:16:48 -0500

    Include textfield and submit button

--- src/OccupancyCounter.js
+++ src/OccupancyCounter.js
@@ -1,25 +1,58 @@
 import React from 'react'
-import './OccupancyCounter.css';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+
+    submitButton: {
+        
+        backgroundColor: "rgb(177, 0, 231)",
+        width: 194, 
+        marginTop: 8
+    }
+
+}
 
 class OccupancyCounter extends React.Component {
 
+    constructor(props) {
+
+        super(props);
+        this.state = {
+
+            isValid: true
+
+        }
+    }
 
     render() {
 
         return(
 
-            <div className="App">
-
-                Counter
-
+            <div className="App" style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
+                <div>
+                    {/* <h2 className="Label-text"> Enter Store Code: &nbsp;&nbsp;  */}
+                    <TextField
+                        error={!this.state.isValid}
+                        id="outlined-error"
+                        label="Enter Store Code"
+                        defaultValue="ABC123"
+                        variant="outlined"
+                    /> 
+                </div>
+                <div>
+                    <Button   
+                        style={styles.submitButton}
+                        variant="contained"
+                        size="large">
+                        Submit
+                    </Button>
+                {/* </h2> */}
+                </div>
             </div>
-
         )
-
     }
-
-
 }
-
 
 export default OccupancyCounter;
