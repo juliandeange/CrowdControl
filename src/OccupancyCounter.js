5ca4303be28a9e3daf82c0b5adb6176f8216846f
Julian De Angelis <julian.deangelis@gmail.com>
2020-12-21 18:14:24 -0500

    Restructure using Grid component

--- src/OccupancyCounter.js
+++ src/OccupancyCounter.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 const styles = {
 
@@ -9,7 +10,14 @@ const styles = {
         
         backgroundColor: "rgb(177, 0, 231)",
         width: 194, 
+        height: 55,
         marginTop: 8
+    },
+
+    // 55
+    componentDimensions: {
+        width: 194,
+        height: 55
     }
 
 }
@@ -26,14 +34,55 @@ class OccupancyCounter extends React.Component {
         }
     }
 
+    
     render() {
+
+        document.body.classList.add("no-sroll")
 
         return(
 
-            <div className="App" style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
-                <div>
+            <div style={{
+                // position: 'absolute', 
+                // left: '50%', 
+                // top: '50%', 
+                // transform: 'translate(-50%, -50%)',
+                // display: 'flex',
+                // height: "100vh", 
+                // width: "100%"
+            }}>
+                <Grid container spacing={3}>
+
+                    <Grid item xs={12}>
+                        <h2 style={{textAlign: "center"}}>Occupancy Counter</h2>
+                    </Grid>
+                    <Grid item xs={12}></Grid>
+                    <Grid item xs={12} style={{textAlign: "center"}}>
+                        <TextField
+                            style={styles.componentDimensions}
+                            error={!this.state.isValid}
+                            id="outlined-error"
+                            label="Enter Store Code"
+                            defaultValue="ABC123"
+                            variant="outlined"
+                        /> 
+                    </Grid>
+                    <Grid item xs={12} style={{textAlign: "center"}}>
+                    <Button   
+                        style={styles.submitButton}
+                        variant="contained"
+                        size="large">
+                        Submit
+                    </Button>
+                    </Grid>
+
+
+                </Grid>
+
+     
+                {/* <div> */}
                     {/* <h2 className="Label-text"> Enter Store Code: &nbsp;&nbsp;  */}
-                    <TextField
+                    {/* <TextField
+                        style={styles.componentDimensions}
                         error={!this.state.isValid}
                         id="outlined-error"
                         label="Enter Store Code"
@@ -48,8 +97,7 @@ class OccupancyCounter extends React.Component {
                         size="large">
                         Submit
                     </Button>
-                {/* </h2> */}
-                </div>
+                </div> */}
             </div>
         )
     }
