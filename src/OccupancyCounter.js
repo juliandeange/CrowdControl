73df998f3d49a4930662c24309d43b2c35b79b60
Julian De Angelis <julian.deangelis@gmail.com>
2020-12-21 18:34:12 -0500

    Cleanup

--- src/OccupancyCounter.js
+++ src/OccupancyCounter.js
@@ -38,23 +38,26 @@ class OccupancyCounter extends React.Component {
     render() {
 
         document.body.classList.add("no-sroll")
+        document.body.classList.add('background-red');
 
         return(
 
-            <div style={{
-                // position: 'absolute', 
+            <div>
+                
+                {/* // position: 'absolute', 
                 // left: '50%', 
                 // top: '50%', 
                 // transform: 'translate(-50%, -50%)',
                 // display: 'flex',
                 // height: "100vh", 
-                // width: "100%"
-            }}>
+                // width: "100%" */}
+
                 <Grid container spacing={3}>
 
                     <Grid item xs={12}>
                         <h2 style={{textAlign: "center"}}>Occupancy Counter</h2>
                     </Grid>
+                    <Grid item xs={12}></Grid>
                     <Grid item xs={12}></Grid>
                     <Grid item xs={12} style={{textAlign: "center"}}>
                         <TextField
@@ -67,37 +70,14 @@ class OccupancyCounter extends React.Component {
                         /> 
                     </Grid>
                     <Grid item xs={12} style={{textAlign: "center"}}>
-                    <Button   
-                        style={styles.submitButton}
-                        variant="contained"
-                        size="large">
-                        Submit
-                    </Button>
+                        <Button   
+                            style={styles.submitButton}
+                            variant="contained"
+                            size="large">
+                            Submit
+                        </Button>
                     </Grid>
-
-
                 </Grid>
-
-     
-                {/* <div> */}
-                    {/* <h2 className="Label-text"> Enter Store Code: &nbsp;&nbsp;  */}
-                    {/* <TextField
-                        style={styles.componentDimensions}
-                        error={!this.state.isValid}
-                        id="outlined-error"
-                        label="Enter Store Code"
-                        defaultValue="ABC123"
-                        variant="outlined"
-                    /> 
-                </div>
-                <div>
-                    <Button   
-                        style={styles.submitButton}
-                        variant="contained"
-                        size="large">
-                        Submit
-                    </Button>
-                </div> */}
             </div>
         )
     }
