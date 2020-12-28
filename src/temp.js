8f622cc68f601b4a345f49d99dd5c990e18e48fa
Julian De Angelis <julian.deangelis@gmail.com>
2020-12-28 17:08:35 -0500

    Create temp.js

---
+++ src/temp.js
@@ -0,0 +1,19 @@
+        // var r = firebase.database().ref("AAA002").on("value", snapshot => {
+        //     snapshot.forEach(snap => {
+        //         // allNotes.push(snap.val());
+        //         console.log(snap.val())
+        //       });
+        //  })
+
+        //Add Node
+        //  var s = firebase.database().ref("AAA002").push(0);
+
+        //Delete Node
+        // firebase.database().ref("AAA002").on("value", snapshot => {
+        //     // console.log(snapshot)
+        //     snapshot.forEach( snap => {
+        //         console.log(snap.key)
+        //         return;
+        //     })
+        // })
+        // var t = firebase.database().ref("AAA002/-MP_CqyrupANbV6MMRnn").remove()
