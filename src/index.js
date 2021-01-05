6f02ca5343d0c084eb8db15bbeab70cb9d0219d1
Julian De Angelis <julian.deangelis@gmail.com>
2021-01-05 18:58:00 -0500

    Update Index.js to use Home component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import OccupancyCounter from './OccupancyCounter';
+import Home from './home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <OccupancyCounter />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
