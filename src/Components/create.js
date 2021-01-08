15e920d67b08aa2dda00499be554baee87dacbe5
Julian De Angelis <julian.deangelis@gmail.com>
2021-01-08 17:55:38 -0500

    Begin dev work for create.js
    Added some text fields to Grid layout

--- src/Components/create.js
+++ src/Components/create.js
@@ -1,11 +1,60 @@
 import React from 'react'
 
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+const styles = {
+
+    componentDimensions: {
+        width: 194,
+        height: 55
+    }
+
+}
+
 class Create extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+
+            isValid: true
+
+        }
+    }
+    
     render() {
 
         return (
-            <div>
+            <div style={{padding: 10}}>
+
+                <Grid container spacing={1}>
+
+                    <Grid item xs={12}>
+                        <TextField
+                            style={styles.componentDimensions}
+                            error={!this.state.isValid}
+                            id="outlined-error"
+                            label="Enter Store Name"
+                            variant="outlined"
+                            value={this.state.storeCode}
+                            onChange={this.storeCodeChanged} 
+                        />
+                    </Grid> 
+
+                    <Grid item xs={12}>
+                        <TextField
+                            style={styles.componentDimensions}
+                            error={!this.state.isValid}
+                            id="outlined-error"
+                            label="Enter Store Code"
+                            variant="outlined"
+                            value={this.state.storeCode}
+                            onChange={this.storeCodeChanged} 
+                        />
+                    </Grid> 
+                </Grid>
 
             </div>
         )
